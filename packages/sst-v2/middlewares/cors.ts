70187ee27ce1fad95dfcb84a7fa9d500ea12d360
Weng Khing <wengkhing@gmail.com>
2025-03-14 21:59:55 +0800

    remove sst-v2 package

--- packages/sst-v2/middlewares/cors.ts
+++
@@ -1,8 +0,0 @@
-import cors from 'cors';
-
-export const gatewayCors = () =>
-  cors({
-    allowedHeaders: ['Content-Type'],
-    credentials: true,
-    origin: JSON.parse(process.env.ALLOWED_ORIGIN as string) as string[],
-  });
