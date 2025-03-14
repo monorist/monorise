a3a60e310d667a2020ea6d9559386463ff5eb46a
Weng Khing <wengkhing@gmail.com>
2025-03-14 22:02:12 +0800

    add aws-cdk package

---
+++ packages/aws-cdk/constructs/monorise-core.ts
@@ -0,0 +1,140 @@
+import { Duration, type Stack } from 'aws-cdk-lib';
+import { EventBus, Rule } from 'aws-cdk-lib/aws-events';
+import { SqsQueue } from 'aws-cdk-lib/aws-events-targets';
+import { Code, type Function as Lambda, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import { EVENT, SOURCE } from './event.js';
+import { QFunction } from './q-function.js';
+import { SingleTable } from './single-table.js';
+
+interface MonoriseCoreProps {
+  appName: string;
+  tableName: string;
+  alarmHandler?: Lambda;
+  coreTable?: SingleTable;
+}
+
+export class MonoriseCore extends Construct {
+  public readonly id: string;
+  public readonly eventBus: EventBus;
+  public readonly coreTable: SingleTable;
+
+  constructor(scope: Stack, id: string, props: MonoriseCoreProps) {
+    super(scope, id);
+
+    this.id = id;
+    this.eventBus = new EventBus(this, 'monorise-event-bus');
+    this.coreTable =
+      props.coreTable ||
+      new SingleTable(scope, props.tableName, {
+        funcProps: {
+          functionName: `${scope.node.tryGetContext('stage')}-${props.appName}-core-replicator`,
+        },
+        tableProps: {
+          timeToLiveAttribute: 'expiresAt',
+        },
+      });
+
+    const environment = {
+      CORE_TABLE: this.coreTable.table.tableName,
+      CORE_EVENT_BUS: this.eventBus.eventBusName,
+    };
+
+    const createEntityProcessor = new QFunction(scope, 'create-entity', {
+      functionProps: {
+        functionName: `${scope.node.tryGetContext('stage')}-${props.appName}-create-entity-processor`,
+        runtime: Runtime.NODEJS_20_X,
+        code: Code.fromAsset('node_modules/@monorise/core/dist/processors'),
+        handler: 'create-entity-processor.handler',
+        timeout: Duration.seconds(30),
+        memorySize: 512,
+        environment,
+      },
+      appName: props.appName,
+    });
+
+    const mutualProcessor = new QFunction(scope, 'mutual', {
+      functionProps: {
+        functionName: `${scope.node.tryGetContext('stage')}-${props.appName}-core-mutual-processor`,
+        runtime: Runtime.NODEJS_20_X,
+        code: Code.fromAsset('node_modules/@monorise/core/dist/processors'),
+        handler: 'mutual-processor.handler',
+        timeout: Duration.seconds(30),
+        memorySize: 512,
+        environment,
+      },
+      appName: props.appName,
+    });
+
+    const prejoinProcessor = new QFunction(scope, 'prejoin', {
+      functionProps: {
+        functionName: `${scope.node.tryGetContext('stage')}-${props.appName}-core-prejoin-processor`,
+        runtime: Runtime.NODEJS_20_X,
+        code: Code.fromAsset('node_modules/@monorise/core/dist/processors'),
+        handler: 'prejoin-processor.handler',
+        timeout: Duration.seconds(30),
+        memorySize: 512,
+        environment,
+      },
+      appName: props.appName,
+    });
+
+    const tagProcessor = new QFunction(scope, 'tag', {
+      functionProps: {
+        functionName: `${scope.node.tryGetContext('stage')}-${props.appName}-core-tag`,
+        runtime: Runtime.NODEJS_20_X,
+        code: Code.fromAsset('node_modules/@monorise/core/dist/processors'),
+        handler: 'tag-processor.handler',
+        timeout: Duration.seconds(30),
+        memorySize: 512,
+        environment,
+      },
+      appName: props.appName,
+    });
+
+    new Rule(scope, 'createEntityRule', {
+      eventBus: this.eventBus,
+      eventPattern: {
+        source: [SOURCE.CORE],
+        detailType: [EVENT.CORE.CREATE_ENTITY.DetailType],
+      },
+      targets: [new SqsQueue(createEntityProcessor.queue)],
+    });
+
+    new Rule(scope, 'mutualProcQueueRule', {
+      eventBus: this.eventBus,
+      eventPattern: {
+        source: [SOURCE.CORE],
+        detailType: [
+          EVENT.CORE.ENTITY_MUTUAL_TO_CREATE.DetailType,
+          EVENT.CORE.ENTITY_MUTUAL_TO_UPDATE.DetailType,
+        ],
+      },
+      targets: [new SqsQueue(mutualProcessor.queue)],
+    });
+
+    new Rule(scope, 'prejoinQueueRule', {
+      eventBus: this.eventBus,
+      eventPattern: {
+        source: [SOURCE.CORE],
+        detailType: [
+          EVENT.CORE.ENTITY_MUTUAL_PROCESSED.DetailType,
+          EVENT.CORE.PREJOIN_RELATIONSHIP_SYNC.DetailType,
+        ],
+      },
+      targets: [new SqsQueue(prejoinProcessor.queue)],
+    });
+
+    new Rule(scope, 'tagQR', {
+      eventBus: this.eventBus,
+      eventPattern: {
+        source: [SOURCE.CORE],
+        detailType: [
+          EVENT.CORE.ENTITY_CREATED.DetailType,
+          EVENT.CORE.ENTITY_UPDATED.DetailType,
+        ],
+      },
+      targets: [new SqsQueue(tagProcessor.queue)],
+    });
+  }
+}
---
+++ packages/aws-cdk/constructs/q-function.ts
@@ -0,0 +1,75 @@
+import { Duration, type Stack } from 'aws-cdk-lib';
+import { Alarm } from 'aws-cdk-lib/aws-cloudwatch';
+import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
+import {
+  Code,
+  type FunctionProps,
+  Function as Lambda,
+  Runtime,
+} from 'aws-cdk-lib/aws-lambda';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import type { Topic } from 'aws-cdk-lib/aws-sns';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
+
+interface QFunctionQueueProps {
+  appName: string;
+  visibilityTimeout?: Duration;
+  maxBatchingWindow?: Duration;
+  batchSize?: number;
+  dlqTopic?: Topic;
+}
+
+interface QFunctionProps extends QFunctionQueueProps {
+  functionProps: FunctionProps;
+}
+
+export class QFunction extends Construct {
+  public readonly id: string;
+  public readonly queue: Queue;
+  private dlq: Queue;
+  private lambdaFunction: Lambda;
+
+  constructor(scope: Stack, id: string, props: QFunctionProps) {
+    super(scope, id);
+
+    this.id = id;
+
+    this.dlq = new Queue(this, `${id}-queue-dlq`, {
+      retentionPeriod: Duration.days(14),
+    });
+
+    this.queue = new Queue(this, `${id}-queue`, {
+      visibilityTimeout: props.visibilityTimeout || Duration.seconds(30),
+      deadLetterQueue: {
+        queue: this.dlq,
+        maxReceiveCount: 3,
+      },
+    });
+
+    this.lambdaFunction = new Lambda(this, `${id}-processor`, {
+      ...props.functionProps,
+    });
+
+    this.lambdaFunction.addEventSource(
+      new SqsEventSource(this.queue, {
+        reportBatchItemFailures: true,
+        batchSize: props.batchSize,
+        maxBatchingWindow: props.maxBatchingWindow,
+      }),
+    );
+
+    const metric = this.dlq.metricApproximateNumberOfMessagesVisible();
+
+    const alarm = new Alarm(this, 'Alarm', {
+      alarmName: `${scope.node.tryGetContext('stage')}-${props.appName}-${id}-queue-dlq-alarm`,
+      metric: metric,
+      threshold: 1,
+      evaluationPeriods: 1,
+    });
+
+    if (props.dlqTopic) {
+      alarm.addAlarmAction(new SnsAction(props.dlqTopic));
+    }
+  }
+}
