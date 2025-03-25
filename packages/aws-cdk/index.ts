b6c43ff1e8091b0eff871ea10d639d1a04cff249
Weng Khing <wengkhing@gmail.com>
2025-03-25 14:49:51 +0800

    tech: remove aws-cdk package

--- packages/aws-cdk/index.ts
+++
@@ -1,5 +0,0 @@
-import { MonoriseCore } from './constructs/monorise-core.js';
-import { QFunction } from './constructs/q-function.js';
-import { SingleTable } from './constructs/single-table.js';
-
-export { QFunction, SingleTable, MonoriseCore };
