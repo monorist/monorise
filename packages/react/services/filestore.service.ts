040f401ae5120ac24508ff2260713a45bd36c497
Jayden Pung <46307126+jaydenpung@users.noreply.github.com>
2025-04-25 19:52:13 +0800

    Update packages/react/services/filestore.service.ts
    Co-authored-by: Weng Khing, Wong <wengkhing@gmail.com>

--- packages/react/services/filestore.service.ts
+++ packages/react/services/filestore.service.ts
@@ -27,7 +27,7 @@ const initFilestoreService = (axios: AxiosInterceptor) => {
   }: UploadFileProps) => {
     const { data } = await axios.get(`${apiBaseUrl}/upload`, {
       requestKey: `filestore/presign/file/${name}`,
-      isInterruptive: disableLoading? false : true,
+      isInterruptive: !disableLoading
       feedback: {
         loading: 'Preparing upload',
       },
